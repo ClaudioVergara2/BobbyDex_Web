0907e31b50ce35944246bf57f3eb89d3dd65d6d5
Claudio Vergara <vergaraclaudio34@gmail.com>
2025-12-05 16:25:39 -0300

    [feat] Se implementan los btn atrás & adelante

--- public/scripts/buttons.js
+++ public/scripts/buttons.js
@@ -1,9 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   const homeBtn = document.querySelector(".home-btn");
+  const prevBtn = document.querySelector(".prev-btn");
+  const nextBtn = document.querySelector(".next-btn");
 
   if (homeBtn) {
     homeBtn.addEventListener("click", () => {
       window.location.href = "/";
+    });
+  }
+
+  if (prevBtn) {
+    prevBtn.addEventListener("click", () => {
+      window.history.back();
+    });
+  }
+
+  if (nextBtn) {
+    nextBtn.addEventListener("click", () => {
+      window.history.forward();
     });
   }
 });
